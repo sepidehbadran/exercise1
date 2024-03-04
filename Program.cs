e4f935d213b5a4a3f1ba8b17d09099c0cfecbe38
Sepideh <sepideh.badran05@gmail.com>
2024-03-04 22:21:12 +0330

    add program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter a number");
+String number = Console.ReadLine();
+int n = int.Parse(number);
+
+for (int i=3; i< n; i = i+3)
+{
+    Console.WriteLine(i);
+}
